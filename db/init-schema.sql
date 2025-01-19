fe227954f92b2176790adfdcdbb3601a24e30423
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-01-19 11:06:02 -0500

    input pipeline basic completed

--- db/init-schema.sql
+++ db/init-schema.sql
@@ -3,8 +3,9 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 -- Tables
 CREATE TABLE main (
-    uuid UUID DEFAULT gen_random_uuid() NOT NULL,
+    uuid UUID DEFAULT gen_random_uuid() NOT NULL UNIQUE,
     text TEXT NOT NULL UNIQUE,
-    embedding VECTOR(3072),
+    embedding VECTOR(3072) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
     PRIMARY KEY (uuid)
 );
--- src/routes/input_data.rs
+++ src/routes/input_data.rs
@@ -7,7 +7,7 @@ use actix_web::{
 use dotenv::dotenv;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use sqlx::{query, query_as};
+use sqlx::QueryBuilder;
 use std::env::var;
 
 #[derive(Debug, Deserialize)]
@@ -45,7 +45,7 @@ async fn input(
         texts: request_body.texts.clone(),
     };
 
-    let embeddings: EmbeddingResponseBody = Client::new()
+    let embedding_response_body: EmbeddingResponseBody = Client::new()
         .post("https://embedder.excoffierleonard.com/embed")
         .bearer_auth(&oai_api_key)
         .json(&embedding_request_body)
@@ -55,7 +55,27 @@ async fn input(
         .json()
         .await?;
 
+    // Create a query builder for bulk insert
+    let mut query_builder = QueryBuilder::new("INSERT INTO main (text, embedding) ");
+
+    // Start the VALUES clause
+    query_builder.push_values(
+        request_body
+            .texts
+            .iter()
+            .zip(embedding_response_body.embeddings.iter()),
+        |mut b, (text, embedding)| {
+            b.push_bind(text).push_bind(embedding);
+        },
+    );
+
+    // Do nothing if the text already exists
+    query_builder.push(" ON CONFLICT (text) DO NOTHING");
+
+    // Execute the bulk insert query
+    query_builder.build().execute(pool.get_pool()).await?;
+
     Ok(HttpResponse::Ok().json(ResponseBody {
-        message: format!("{:?}", embeddings),
+        message: "Success".to_string(),
     }))
 }
